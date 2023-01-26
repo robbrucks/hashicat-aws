ff900026d1b85961127259f7f3447c57bddc99c2
Rob Brucks <robbrucks@users.noreply.github.com>
2023-01-26 13:06:52 -0600

    make-it-my-own
    Making it my own

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own.
+  I Say We Take Off. Nuke The Site From Orbit. It's The Only Way To Be Sure.
   <!-- END -->
 
   </div>
